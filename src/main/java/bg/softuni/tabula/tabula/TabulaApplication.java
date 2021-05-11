8d7fd51e3f013e2de0e08c36d52cf6d8ca228ed7
Plamen Dragiev <pl.dragiev@gmail.com>
2021-05-11 16:51:59 +0300

    Adding spring boot project created from spring.io site

---
+++ src/main/java/bg/softuni/tabula/tabula/TabulaApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.tabula.tabula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/tabula/tabula/TabulaApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.tabula.tabula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
